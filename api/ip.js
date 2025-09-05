e9878e7fd82fd8300f006a2f4d9972ed5951f045
emppu1 <87761811+emppu1@users.noreply.github.com>
2025-09-05 12:43:15 +0300

    Delete api directory

--- api/ip.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  const ip =
-    req.headers['x-forwarded-for']?.split(',')[0] ||
-    req.connection?.remoteAddress ||
-    req.socket?.remoteAddress ||
-    req.connection?.socket?.remoteAddress ||
-    'unknown';
-  res.status(200).json({ ip });
-}
